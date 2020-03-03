de7e0791bc39fe1ed7bafb68f9864ed011c5a4f4
Alberto Hernandez Angel <alberto@gobierno.io>
2020-03-03 04:06:15 -0600

    Se actualizaron los proyectos

--- index.php
+++ index.php
@@ -2,55 +2,63 @@
 
     <!-- =========================      CAROUSEL           ================================ -->
     <div class="container main">
-        <div id="carouselExampleSlidesOnly" class="carousel slide pt-3 pb-3" data-ride="carousel">
+        <div id="carouselPrincipal" class="carousel slide pt-3 pb-3" data-ride="carousel">
 
         <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+            <li data-target="#carouselPrincipal" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselPrincipal" data-slide-to="1"></li>
+            <li data-target="#carouselPrincipal" data-slide-to="2"></li>
+            <li data-target="#carouselPrincipal" data-slide-to="3"></li>
+            <li data-target="#carouselPrincipal" data-slide-to="4"></li>
+            <li data-target="#carouselPrincipal" data-slide-to="5"></li>
+            <li data-target="#carouselPrincipal" data-slide-to="6"></li>
+            <li data-target="#carouselPrincipal" data-slide-to="7"></li>
         </ol>
 
 
         <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img class="d-block w-100" src="assets/img/slider/slider7.jpg">
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src="assets/img/slider/slider6.jpg">
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src="assets/img/slider/slider1.jpg">
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src="assets/img/slider/slider5.jpg">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="assets/img/slider/slider8.jpg">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="assets/img/slider/slider9.jpg">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="assets/img/slider/slider10.jpg">
-            </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src="assets/img/slider/slider11.jpg">
             </div>
+            
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="assets/img/slider/slider10.jpg">
+            </div>
+
+
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/slider/slider7.jpg">
+            </div>
+
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/slider/slider6.jpg">
+            </div>
+
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/slider/slider1.jpg">
+            </div>
+
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/slider/slider5.jpg">
+            </div>
+
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/slider/slider8.jpg">
+            </div>
+
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/slider/slider9.jpg">
+            </div>
         </div>
 
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+                <a class="carousel-control-prev" href="#carouselPrincipal" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselPrincipal" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
         </div>
 
 
